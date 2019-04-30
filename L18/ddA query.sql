c81946eb5182462838f9159f2c79ac4723f12453
Tom Steinmeier <neuro.genesis67@gmail.com>
2019-04-30 11:41:47 +0200

    lp

--- L18/ddA query.sql
+++ L18/ddA query.sql
@@ -1,5 +1,7 @@
 drop table kunde
 drop table bestilling
+drop view kundeView
+---------------------------
 
 ---------------------------
 create table kunde (
@@ -13,22 +15,27 @@ varenavn varchar(25),
 antalVarer int);
 ---------------------------
 
+
 ---------------------------
 insert into kunde values (1, 'Pedro', 8920);
-insert into kunde values (2, 'Xiaoyu', 7800);
-insert into kunde values (3, 'Nina', 6760);
+insert into kunde values (3, 'Xiaoyu', 7800);
+insert into kunde values (5, 'Nina', 6760);
 
-insert into bestilling values (4, 'Hæksaks', 22);
-insert into bestilling values (5, 'Bælte', 1);
-insert into bestilling values (6, 'Faldskærm', 3);
+insert into bestilling values (1, 'Hæksaks', 1);
+insert into bestilling values (2, 'Bælte', 3);
+insert into bestilling values (3, 'Faldskærm', 5);
+insert into bestilling values (4, 'Ulddyr', 200);
+insert into bestilling values (5, 'Elcykel', 1);
+insert into bestilling values (6, 'Mascara', 3);
 ---------------------------
 
 ---------------------------
-create view kundeView AS 
-SELECT * FROM kunde
-UNION 
-SELECT * FROM ddB.dbo.kunde 
----------------------------
+go
+create view kundeView as 
+select * from kunde
+union
+select * from ddB.dbo.kunde
+--------------------------
 
 ---------------------------
 CREATE TRIGGER kundeViewTrigger 
@@ -40,12 +47,24 @@ BEGIN
 	INSERT INTO kunde(kundeid, navn, postnr)
 	SELECT i.kundeid, i.navn, i.postnr 
 	FROM INSERTED i
+
 	ELSE 
 	INSERT INTO ddB.dbo.kunde(kundeid, navn, postnr)
 	SELECT i.kundeid, i.navn, i.postnr 
 	FROM INSERTED i
 	END
 
-	INSERT INTO kundeView values (7, 'Karsten gråhår', 9999)
+	--INSERT INTO kundeView values (7, 'Karsten gråhår', 9999)
+	--delete from ddB.dbo.kunde where kundeid = 7;
 ---------------------------
+-- Summen af alle bestillinger --
+--Code goes here
 
+
+
+
+
+---------------------------
+SELECT * FROM kunde
+select * from bestilling
+SELECT * FROM ddB.dbo.kunde 
--- L18/ddB query.sql
+++ L18/ddB query.sql
@@ -1,5 +1,6 @@
 drop table kunde
 drop table postering
+drop view kundeView
 
 create table kunde (
 kundeid int primary key,
@@ -10,28 +11,31 @@ create table postering (
 kundeid int, 
 beloeb int);
 
-insert into kunde values (4, 'Sombra', 6200);
-insert into kunde values (5, 'Shaniqua', 7261);
-insert into kunde values (6, 'Shaheen', 2661);
-
-insert into postering values (1, 200);
-insert into postering values (2, 400);
-insert into postering values (3, 600);
-
----------------------------
+----------------------------
+go
 create view kundeView as 
-select kundeid, navn, postnr 
-from kunde
-
-drop view kundeView
-
-select * from kundeView
+select * from kunde
+union
+select * from ddA.dbo.kunde
 --------------------------
 
+insert into kunde values (2, 'Sombra', 6200);
+insert into kunde values (4, 'Shaniqua', 7261);
+insert into kunde values (6, 'Shaheen', 2661);
+
+insert into postering values (1, 100);
+insert into postering values (2, 200);
+insert into postering values (3, 300);
+insert into postering values (4, 400);
+insert into postering values (5, 500);
+insert into postering values (6, 600);
+
+--------------------------
+--Lav en query på både database A og B, der finder alle kunder og summen af deres posteringer.
+--Code goes here
 
 
-
-
-
+-----practical-----
+select * from kundeView
 select * from kunde
 select * from postering
